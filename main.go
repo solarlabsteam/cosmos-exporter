833a3a857515c76c376a2344a55cd1ff2ed3a53f
Techno Freak <freak12techno@gmail.com>
2021-04-19 19:09:28 +0300

    feat: refactor + add redelegations

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "context"
-    "net/http"
-    "google.golang.org/grpc"
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
-    "flag"
-    "sync"
+	"context"
+	"flag"
+	"net/http"
+	"sync"
 
-    log "github.com/sirupsen/logrus"
-    sdk "github.com/cosmos/cosmos-sdk/types"
-    banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-    stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	log "github.com/sirupsen/logrus"
 )
 
 var prefix = flag.String("bech-prefix", "persistence", "Bech32 prefix for the network")
@@ -22,151 +22,187 @@ var listenAddress = flag.String("listen-address", ":9300", "The address this exp
 var nodeAddress = flag.String("node", "localhost:9090", "RPC node address")
 
 func walletHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.ClientConn) {
-    address :=  r.URL.Query().Get("address")
-    myAddress, err := sdk.AccAddressFromBech32(address)
-    if err != nil {
-        log.Error("Could not get address for \"", address, "\", got error: ", err)
-        return
-    }
+	address := r.URL.Query().Get("address")
+	myAddress, err := sdk.AccAddressFromBech32(address)
+	if err != nil {
+		log.Error("Could not get address for \"", address, "\", got error: ", err)
+		return
+	}
 
+	walletBalanceGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_wallet_balance",
+			Help: "Balance of the Cosmos-based blockchain wallet",
+		},
+		[]string{"address", "denom"},
+	)
 
-    walletBalanceGauge := prometheus.NewGaugeVec(
-        prometheus.GaugeOpts{
-            Name: "cosmos_wallet_balance",
-            Help: "Balance of the Cosmos-based blockchain wallet",
-        },
-        []string{"address", "denom"},
-    )
+	walletDelegationGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_wallet_delegations",
+			Help: "Delegations of the Cosmos-based blockchain wallet",
+		},
+		[]string{"address", "denom", "delegated_to"},
+	)
 
-    walletDelegationGauge := prometheus.NewGaugeVec(
-        prometheus.GaugeOpts{
-            Name: "cosmos_wallet_delegations",
-            Help: "Delegations of the Cosmos-based blockchain wallet",
-        },
-        []string{"address", "denom", "delegated_to"},
-    )
+	walletRedelegationGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_wallet_redelegations",
+			Help: "Redlegations of the Cosmos-based blockchain wallet",
+		},
+		[]string{"address", "denom", "redelegated_from", "redelegated_to"},
+	)
 
-    walletUnbondingsGauge := prometheus.NewGaugeVec(
-        prometheus.GaugeOpts{
-            Name: "cosmos_wallet_unbondings",
-            Help: "Unbondings of the Cosmos-based blockchain wallet",
-        },
-        []string{"address", "denom", "unbonded_from"},
-    )
+	walletUnbondingsGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_wallet_unbondings",
+			Help: "Unbondings of the Cosmos-based blockchain wallet",
+		},
+		[]string{"address", "denom", "unbonded_from"},
+	)
 
-    registry := prometheus.NewRegistry()
-    registry.MustRegister(walletBalanceGauge)
-    registry.MustRegister(walletDelegationGauge)
-    registry.MustRegister(walletUnbondingsGauge)
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(walletBalanceGauge)
+	registry.MustRegister(walletDelegationGauge)
+	registry.MustRegister(walletUnbondingsGauge)
+	registry.MustRegister(walletRedelegationGauge)
 
-    
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-    go func() {
-        defer wg.Done()
+	go func() {
+		defer wg.Done()
 
-        bankClient := banktypes.NewQueryClient(grpcConn)
-        bankRes, err := bankClient.Balance(
-            context.Background(),
-            &banktypes.QueryBalanceRequest{Address: myAddress.String(), Denom: *denom},
-        )
-        if err != nil {
-            log.Error("Could not get balance for \"", address, "\", got error: ", err)
-            return
-        }
+		bankClient := banktypes.NewQueryClient(grpcConn)
+		bankRes, err := bankClient.Balance(
+			context.Background(),
+			&banktypes.QueryBalanceRequest{Address: myAddress.String(), Denom: *denom},
+		)
+		if err != nil {
+			log.Error("Could not get balance for \"", address, "\", got error: ", err)
+			return
+		}
 
-        walletBalanceGauge.With(prometheus.Labels{
-            "address": address,
-            "denom": bankRes.GetBalance().Denom,
-        }).Set(float64(bankRes.GetBalance().Amount.Int64()))
-    }()
-    wg.Add(1)
+		walletBalanceGauge.With(prometheus.Labels{
+			"address": address,
+			"denom":   bankRes.GetBalance().Denom,
+		}).Set(float64(bankRes.GetBalance().Amount.Int64()))
+	}()
+	wg.Add(1)
 
-    go func() {
-        defer wg.Done()
+	go func() {
+		defer wg.Done()
 
-        stakingClient := stakingtypes.NewQueryClient(grpcConn)
-        stakingRes, err := stakingClient.DelegatorDelegations(
-            context.Background(),
-            &stakingtypes.QueryDelegatorDelegationsRequest{DelegatorAddr: myAddress.String()},
-        )
-        if err != nil {
-            log.Error("Could not get delegations for \"", address, "\", got error: ", err)
-            return
-        }
+		stakingClient := stakingtypes.NewQueryClient(grpcConn)
+		stakingRes, err := stakingClient.DelegatorDelegations(
+			context.Background(),
+			&stakingtypes.QueryDelegatorDelegationsRequest{DelegatorAddr: myAddress.String()},
+		)
+		if err != nil {
+			log.Error("Could not get delegations for \"", address, "\", got error: ", err)
+			return
+		}
 
-        for _, delegation := range stakingRes.DelegationResponses {
-            walletDelegationGauge.With(prometheus.Labels{
-                "address": address,
-                "denom": delegation.Balance.Denom,
-                "delegated_to": delegation.Delegation.ValidatorAddress,
-            }).Set(float64(delegation.Balance.Amount.Int64()))
-        }
-    }()
-    wg.Add(1)
+		for _, delegation := range stakingRes.DelegationResponses {
+			walletDelegationGauge.With(prometheus.Labels{
+				"address":      address,
+				"denom":        delegation.Balance.Denom,
+				"delegated_to": delegation.Delegation.ValidatorAddress,
+			}).Set(float64(delegation.Balance.Amount.Int64()))
+		}
+	}()
+	wg.Add(1)
 
-    go func() {
-        defer wg.Done()
+	go func() {
+		defer wg.Done()
 
-        stakingClient := stakingtypes.NewQueryClient(grpcConn)
-        stakingRes, err := stakingClient.DelegatorUnbondingDelegations(
-            context.Background(),
-            &stakingtypes.QueryDelegatorUnbondingDelegationsRequest{DelegatorAddr: myAddress.String()},
-        )
-        if err != nil {
-            log.Error("Could not get unbonding delegations for \"", address, "\", got error: ", err)
-            return
-        }
+		stakingClient := stakingtypes.NewQueryClient(grpcConn)
+		stakingRes, err := stakingClient.DelegatorUnbondingDelegations(
+			context.Background(),
+			&stakingtypes.QueryDelegatorUnbondingDelegationsRequest{DelegatorAddr: myAddress.String()},
+		)
+		if err != nil {
+			log.Error("Could not get unbonding delegations for \"", address, "\", got error: ", err)
+			return
+		}
 
-        for _, unbonding := range stakingRes.UnbondingResponses {
-            var sum float64 = 0
-            for _, entry := range unbonding.Entries {
-                sum += float64(entry.Balance.Int64())
-            }
+		for _, unbonding := range stakingRes.UnbondingResponses {
+			var sum float64 = 0
+			for _, entry := range unbonding.Entries {
+				sum += float64(entry.Balance.Int64())
+			}
 
-            walletUnbondingsGauge.With(prometheus.Labels{
-                "address": unbonding.DelegatorAddress,
-                "denom": *denom, // unbonding does not have denom in response for some reason
-                "unbonded_from": unbonding.ValidatorAddress,
-            }).Set(sum)
-        }
-    }()
-    wg.Add(1)
+			walletUnbondingsGauge.With(prometheus.Labels{
+				"address":       unbonding.DelegatorAddress,
+				"denom":         *denom, // unbonding does not have denom in response for some reason
+				"unbonded_from": unbonding.ValidatorAddress,
+			}).Set(sum)
+		}
+	}()
+	wg.Add(1)
 
-    wg.Wait()
+	go func() {
+		defer wg.Done()
 
-    h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
-    h.ServeHTTP(w, r)
-    log.Info("GET /metrics/wallet?address=", address)
+		stakingClient := stakingtypes.NewQueryClient(grpcConn)
+		stakingRes, err := stakingClient.Redelegations(
+			context.Background(),
+			&stakingtypes.QueryRedelegationsRequest{DelegatorAddr: myAddress.String()},
+		)
+		if err != nil {
+			log.Error("Could not get redelegations for \"", address, "\", got error: ", err)
+			return
+		}
+
+		for _, redelegation := range stakingRes.RedelegationResponses {
+			var sum float64 = 0
+			for _, entry := range redelegation.Entries {
+				sum += float64(entry.Balance.Int64())
+			}
+
+			walletRedelegationGauge.With(prometheus.Labels{
+				"address":          redelegation.Redelegation.DelegatorAddress,
+				"denom":            *denom, // redelegation does not have denom in response for some reason
+				"redelegated_from": redelegation.Redelegation.ValidatorSrcAddress,
+				"redelegated_to":   redelegation.Redelegation.ValidatorDstAddress,
+			}).Set(sum)
+		}
+	}()
+	wg.Add(1)
+
+	wg.Wait()
+
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+	log.Info("GET /metrics/wallet?address=", address)
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    config := sdk.GetConfig()
-    config.SetBech32PrefixForAccount(*prefix, *prefix + "pub")
-    config.SetBech32PrefixForValidator(*prefix + "valoper", *prefix + "valoperpub")
-    config.SetBech32PrefixForConsensusNode(*prefix + "valcons", *prefix + "valconspub")
-    config.Seal()
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount(*prefix, *prefix+"pub")
+	config.SetBech32PrefixForValidator(*prefix+"valoper", *prefix+"valoperpub")
+	config.SetBech32PrefixForConsensusNode(*prefix+"valcons", *prefix+"valconspub")
+	config.Seal()
 
-    grpcConn, err := grpc.Dial(
-        *nodeAddress,
-        grpc.WithInsecure(),
-    )
-    if err != nil {
-        panic(err);
-    }
+	grpcConn, err := grpc.Dial(
+		*nodeAddress,
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		panic(err)
+	}
 
-    defer grpcConn.Close()
+	defer grpcConn.Close()
 
-    http.HandleFunc("/metrics/wallet", func(w http.ResponseWriter, r *http.Request) {
-        walletHandler(w, r, grpcConn)
-    })
+	http.HandleFunc("/metrics/wallet", func(w http.ResponseWriter, r *http.Request) {
+		walletHandler(w, r, grpcConn)
+	})
 
-    log.Info("Listening on ", *listenAddress)
-    err = http.ListenAndServe(*listenAddress, nil)
-    if err != nil {
-        log.Fatal("Could not start application at ", *listenAddress, ", got error: ", err)
-        panic(err)
-    }
+	log.Info("Listening on ", *listenAddress)
+	err = http.ListenAndServe(*listenAddress, nil)
+	if err != nil {
+		log.Fatal("Could not start application at ", *listenAddress, ", got error: ", err)
+		panic(err)
+	}
 }

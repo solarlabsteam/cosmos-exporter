f66c3a6a31e78b565a973c7922bc211b0155e1a5
Techno Freak <freak12techno@gmail.com>
2021-04-20 12:27:41 +0300

    feat: validators metrics

--- main.go
+++ main.go
@@ -318,7 +318,7 @@ func validatorHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.Cli
 			&stakingtypes.QueryValidatorRequest{ValidatorAddr: myAddress.String()},
 		)
 		if err != nil {
-			log.Error("Could not get delegations for \"", address, "\", got error: ", err)
+			log.Error("Could not get validator for \"", address, "\", got error: ", err)
 			return
 		}
 
@@ -428,6 +428,119 @@ func validatorHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.Cli
 	log.Info("GET /metrics/validator?address=", address)
 }
 
+func validatorsHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.ClientConn) {
+	validatorsCommissionGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_validators_commission",
+			Help: "Commission of the Cosmos-based blockchain validator",
+		},
+		[]string{"address", "denom"},
+	)
+
+	validatorsStatusGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_validators_status",
+			Help: "Status of the Cosmos-based blockchain validator",
+		},
+		[]string{"address"},
+	)
+
+	validatorsJailedGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_validators_jailed",
+			Help: "Jailed status of the Cosmos-based blockchain validator",
+		},
+		[]string{"address"},
+	)
+
+	validatorsTokensGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_validators_tokens",
+			Help: "Tokens of the Cosmos-based blockchain validator",
+		},
+		[]string{"address"},
+	)
+
+	validatorsDelegatorSharesGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_validators_delegator_shares",
+			Help: "Delegator shares of the Cosmos-based blockchain validator",
+		},
+		[]string{"address"},
+	)
+
+	validatorsMinSelfDelegationGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_validators_min_self_delegation",
+			Help: "Self declared minimum self delegation shares of the Cosmos-based blockchain validator",
+		},
+		[]string{"address"},
+	)
+
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(validatorsCommissionGauge)
+	registry.MustRegister(validatorsStatusGauge)
+	registry.MustRegister(validatorsJailedGauge)
+	registry.MustRegister(validatorsTokensGauge)
+	registry.MustRegister(validatorsDelegatorSharesGauge)
+	registry.MustRegister(validatorsMinSelfDelegationGauge)
+
+	stakingClient := stakingtypes.NewQueryClient(grpcConn)
+	validators, err := stakingClient.Validators(
+		context.Background(),
+		&stakingtypes.QueryValidatorsRequest{},
+	)
+	if err != nil {
+		log.Error("Could not get validators, got error: ", err)
+		return
+	}
+
+	for _, validator := range validators.Validators {
+		// because cosmos's dec doesn't have .toFloat64() method or whatever and returns everything as int
+		rate, err := strconv.ParseFloat(validator.Commission.CommissionRates.Rate.String(), 64)
+		if err != nil {
+			log.Error("Could not get commission rate for \"", validator.OperatorAddress, "\", got error: ", err)
+		} else {
+			validatorsCommissionGauge.With(prometheus.Labels{
+				"address": validator.OperatorAddress,
+				"denom":   *denom,
+			}).Set(rate)
+		}
+
+		validatorsStatusGauge.With(prometheus.Labels{
+			"address": validator.OperatorAddress,
+		}).Set(float64(validator.Status))
+
+		// golang doesn't have a ternary operator, so we have to stick with this ugly solution
+		var jailed float64
+
+		if validator.Jailed {
+			jailed = 1
+		} else {
+			jailed = 0
+		}
+		validatorsJailedGauge.With(prometheus.Labels{
+			"address": validator.OperatorAddress,
+		}).Set(float64(jailed))
+
+		validatorsTokensGauge.With(prometheus.Labels{
+			"address": validator.OperatorAddress,
+		}).Set(float64(validator.Tokens.Int64()))
+
+		validatorsDelegatorSharesGauge.With(prometheus.Labels{
+			"address": validator.OperatorAddress,
+		}).Set(float64(validator.DelegatorShares.RoundInt64()))
+
+		validatorsMinSelfDelegationGauge.With(prometheus.Labels{
+			"address": validator.OperatorAddress,
+		}).Set(float64(validator.MinSelfDelegation.Int64()))
+	}
+
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+	log.Info("GET /metrics/validators")
+}
+
 func main() {
 	flag.Parse()
 
@@ -453,6 +566,10 @@ func main() {
 
 	http.HandleFunc("/metrics/validator", func(w http.ResponseWriter, r *http.Request) {
 		validatorHandler(w, r, grpcConn)
+	})
+
+	http.HandleFunc("/metrics/validators", func(w http.ResponseWriter, r *http.Request) {
+		validatorsHandler(w, r, grpcConn)
 	})
 
 	log.Info("Listening on ", *listenAddress)

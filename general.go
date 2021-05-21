b2d8a8306cace73a6bec6611f464c23f60369f18
Techno Freak <freak12techno@gmail.com>
2021-05-21 14:39:35 +0300

    feat: added community pool

--- general.go
+++ general.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 
+	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
@@ -35,9 +37,18 @@ func GeneralHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.Clien
 		},
 	)
 
+	generalCommunityPoolGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cosmos_general_community_pool",
+			Help: "Community pool",
+		},
+		[]string{"denom"},
+	)
+
 	registry := prometheus.NewRegistry()
 	registry.MustRegister(generalBondedTokensGauge)
 	registry.MustRegister(generalNotBondedTokensGauge)
+	registry.MustRegister(generalCommunityPoolGauge)
 
 	var wg sync.WaitGroup
 
@@ -62,6 +73,39 @@ func GeneralHandler(w http.ResponseWriter, r *http.Request, grpcConn *grpc.Clien
 
 		generalBondedTokensGauge.Set(float64(response.Pool.BondedTokens.Int64()))
 		generalNotBondedTokensGauge.Set(float64(response.Pool.NotBondedTokens.Int64()))
+	}()
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		sublogger.Debug().Msg("Started querying distribution community pool")
+		queryStart := time.Now()
+
+		distributionClient := distributiontypes.NewQueryClient(grpcConn)
+		response, err := distributionClient.CommunityPool(
+			context.Background(),
+			&distributiontypes.QueryCommunityPoolRequest{},
+		)
+		if err != nil {
+			sublogger.Error().Err(err).Msg("Could not get distribution community pool")
+			return
+		}
+
+		sublogger.Debug().
+			Float64("request-time", time.Since(queryStart).Seconds()).
+			Msg("Finished querying distribution community pool")
+
+		for _, coin := range response.Pool {
+			if value, err := strconv.ParseFloat(coin.Amount.String(), 64); err != nil {
+				sublogger.Error().
+					Err(err).
+					Msg("Could not get community pool coin")
+			} else {
+				generalCommunityPoolGauge.With(prometheus.Labels{
+					"denom": coin.Denom,
+				}).Set(value)
+			}
+		}
 	}()
 	wg.Add(1)
 

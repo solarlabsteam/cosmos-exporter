60d4b79fd49576c7d4322c976c446699ee6c8cc7
Konstantin <crckostage@gmail.com>
2022-07-06 15:37:20 +0300

    add tls support from node exporter

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 	"net/http"
 	"os"
 
+	nelog "github.com/go-kit/log"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/exporter-toolkit/web"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -18,7 +22,8 @@ import (
 )
 
 var (
-	ConfigPath string
+	ConfigPath    string
+	WebConfigPath string
 
 	Denom         string
 	ListenAddress string
@@ -138,6 +143,7 @@ func Execute(cmd *cobra.Command, args []string) {
 		Str("--listen-address", ListenAddress).
 		Str("--node", NodeAddress).
 		Str("--log-level", LogLevel).
+		Str("--web-config", WebConfigPath).
 		Msg("Started with following parameters")
 
 	config := sdk.GetConfig()
@@ -157,29 +163,24 @@ func Execute(cmd *cobra.Command, args []string) {
 	setChainID()
 	setDenom(grpcConn)
 
-	http.HandleFunc("/metrics/wallet", func(w http.ResponseWriter, r *http.Request) {
-		WalletHandler(w, r, grpcConn)
-	})
-
-	http.HandleFunc("/metrics/validator", func(w http.ResponseWriter, r *http.Request) {
-		ValidatorHandler(w, r, grpcConn)
-	})
-
-	http.HandleFunc("/metrics/validators", func(w http.ResponseWriter, r *http.Request) {
-		ValidatorsHandler(w, r, grpcConn)
-	})
-
-	http.HandleFunc("/metrics/params", func(w http.ResponseWriter, r *http.Request) {
-		ParamsHandler(w, r, grpcConn)
-	})
-
-	http.HandleFunc("/metrics/general", func(w http.ResponseWriter, r *http.Request) {
-		GeneralHandler(w, r, grpcConn)
-	})
+	makeHandler := func(
+		handler func(http.ResponseWriter, *http.Request, *grpc.ClientConn),
+		grpcConn *grpc.ClientConn,
+	) func(http.ResponseWriter, *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			handler(w, r, grpcConn)
+		}
+	}
+	router := mux.NewRouter()
+	router.HandleFunc("/metrics/wallet", makeHandler(WalletHandler, grpcConn))
+	router.HandleFunc("/metrics/validator", makeHandler(ValidatorHandler, grpcConn))
+	router.HandleFunc("/metrics/validators", makeHandler(ValidatorsHandler, grpcConn))
+	router.HandleFunc("/metrics/params", makeHandler(ParamsHandler, grpcConn))
+	router.HandleFunc("/metrics/general", makeHandler(GeneralHandler, grpcConn))
 
 	log.Info().Str("address", ListenAddress).Msg("Listening")
-	err = http.ListenAndServe(ListenAddress, nil)
-	if err != nil {
+	server := &http.Server{Addr: ListenAddress, Handler: router}
+	if err := web.ListenAndServe(server, WebConfigPath, nelog.NewLogfmtLogger(os.Stdout)); err != nil {
 		log.Fatal().Err(err).Msg("Could not start application")
 	}
 }
@@ -251,6 +252,7 @@ func setDenom(grpcConn *grpc.ClientConn) {
 
 func main() {
 	rootCmd.PersistentFlags().StringVar(&ConfigPath, "config", "", "Config file path")
+	rootCmd.PersistentFlags().StringVar(&WebConfigPath, "web-config", "", "TLS config file path")
 	rootCmd.PersistentFlags().StringVar(&Denom, "denom", "", "Cosmos coin denom")
 	rootCmd.PersistentFlags().Float64Var(&DenomCoefficient, "denom-coefficient", 0, "Denom coefficient")
 	rootCmd.PersistentFlags().StringVar(&ListenAddress, "listen-address", ":9300", "The address this exporter would listen on")

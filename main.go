e7c0b6b1cc5de3efe822d11c21b79a54ebfbd92d
Adrien Touzouli <a.touzouli@okp4.com>
2022-12-01 10:45:56 +0100

    style: make gofumpt happy

--- main.go
+++ main.go
@@ -262,7 +262,6 @@ func setDenom(grpcConn *grpc.ClientConn) {
 }
 
 func checkAndHandleDenomInfoProvidedByUser() bool {
-
 	if Denom != "" {
 		if DenomCoefficient != 1 && DenomExponent != 0 {
 			log.Fatal().Msg("denom-coefficient and denom-exponent are both provided. Must provide only one")
@@ -290,7 +289,6 @@ func checkAndHandleDenomInfoProvidedByUser() bool {
 	}
 
 	return false
-
 }
 
 func main() {
